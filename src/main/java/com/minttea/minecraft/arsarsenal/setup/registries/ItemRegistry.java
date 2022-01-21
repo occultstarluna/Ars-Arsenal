7b22150ff28207654854390c192b281ae6bcbf44
fdavis97 <85691166+fdavis97@users.noreply.github.com>
2022-01-21 16:01:03 -0600

    started the stuff for source steel tools!

--- src/main/java/com/minttea/minecraft/arsarsenal/setup/registries/ItemRegistry.java
+++ src/main/java/com/minttea/minecraft/arsarsenal/setup/registries/ItemRegistry.java
@@ -32,6 +32,7 @@ public class ItemRegistry {
     @ObjectHolder("source_steel_leggings")public static SourceSteelArmor sourceSteelLeggings;
     @ObjectHolder("source_steel_boots")public static SourceSteelArmor sourceSteelBoots;
     @ObjectHolder("fire_hat")public static FireHat fireHat;
+    @ObjectHolder("fire_hood")public static FireHat fireHood;
     @ObjectHolder("fire_robes")public static FireRobes fireRobe;
     @ObjectHolder("fire_leggings")public static FireRobes fireLegs;
     @ObjectHolder("fire_boots")public static FireRobes fireBoots;
@@ -39,7 +40,8 @@ public class ItemRegistry {
     @ObjectHolder("aqua_robes")public static AquaRobes aquaRobe;
     @ObjectHolder("aqua_leggings")public static AquaRobes aquaLegs;
     @ObjectHolder("aqua_boots")public static AquaRobes aquaBoots;
-    @ObjectHolder("earth_hat")public static EarthHat earthHood;
+    @ObjectHolder("earth_hat")public static EarthHat earthHat;
+    @ObjectHolder("earth_hood")public static EarthHat earthHood;
     @ObjectHolder("earth_robes")public static EarthRobes earthRobe;
     @ObjectHolder("earth_leggings")public static EarthRobes earthLegs;
     @ObjectHolder("earth_boots")public static EarthRobes earthBoots;
@@ -73,6 +75,7 @@ public class ItemRegistry {
         registry.register(new SourceSteelArmor(EquipmentSlotType.FEET).setRegistryName("source_steel_boots"));
 
         registry.register(new FireHat().setRegistryName("fire_hat"));
+        registry.register(new FireRobes(EquipmentSlotType.HEAD).setRegistryName("fire_hood"));
         registry.register(new FireRobes(EquipmentSlotType.CHEST).setRegistryName("fire_robes"));
         registry.register(new FireRobes(EquipmentSlotType.LEGS).setRegistryName("fire_leggings"));
         registry.register(new FireRobes(EquipmentSlotType.FEET).setRegistryName("fire_boots"));
@@ -83,6 +86,7 @@ public class ItemRegistry {
         registry.register(new AquaRobes(EquipmentSlotType.FEET).setRegistryName("aqua_boots"));
 
         registry.register(new EarthHat().setRegistryName("earth_hat"));
+        registry.register(new EarthRobes(EquipmentSlotType.HEAD).setRegistryName("earth_hood"));
         registry.register(new EarthRobes(EquipmentSlotType.CHEST).setRegistryName("earth_robes"));
         registry.register(new EarthRobes(EquipmentSlotType.LEGS).setRegistryName("earth_leggings"));
         registry.register(new EarthRobes(EquipmentSlotType.FEET).setRegistryName("earth_boots"));
